fd7da38d6e5eba6d22bd02833fbb12b18aebe286
AndiLaptop <andip@LAPTOP-IKGH1AAO>
2022-09-15 17:41:26 -0500

    working on reading file

--- COMP303_Assign_1/COMP303_Assign_1.cpp
+++ COMP303_Assign_1/COMP303_Assign_1.cpp
@@ -8,7 +8,30 @@ using namespace std;
 
 int main()
 {
+	fstream user_file;
+	string filename;
+	const int ARRAY_SIZE = 10;
+	int intArray[ARRAY_SIZE];
 
-	read_file();
+	cout << "Please enter name of file: ";
+	cin >> filename;
+
+	user_file.open(filename);
+
+	while (!user_file.is_open()) {
+		cout << "Error: file not found." << endl;
+		cout << endl;
+		cout << "Please enter name of file: ";
+
+		cin >> filename;
+
+		user_file.open(filename);
+
+	}
+
+	for (int i = 0; i < ARRAY_SIZE; i++) {
+		getline(user_file, intArray[i]);
+	}
+
 
 }
--- COMP303_Assign_1/IntHandling.cpp
+++ COMP303_Assign_1/IntHandling.cpp
@@ -6,24 +6,3 @@
 
 using namespace std;
 
-void read_file() {
-	fstream user_file;
-	string filename;
-
-	cout << "Enter name of file: ";
-	cin >> filename;
-
-	user_file.open(filename);
-
-	while (!user_file.is_open()) {
-		cout << "Error: file not found." << endl;
-		cout << "Please enter another filename: ";
-
-		cin >> filename;
-
-		user_file.open(filename);
-
-	}
-
-
-}
